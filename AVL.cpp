5bf8e07ccd53cd73c491051aab01f50bb77b8b09
Jorge <jorger@tec.mx>
2021-11-05 15:07:39 -0600

    Agregado AVL final

--- AVL.cpp
+++ AVL.cpp
@@ -26,42 +26,84 @@ class Nodo{
 };
 
 template <class T>
-class BST{
+class AVL{
 	public:
 	Nodo<T> *root;
 	
-	BST(){
+	AVL(){
 		root=NULL;
 	}
 	
+	int altura(Nodo<T> *it){
+		if(it==NULL){
+			return -1;
+		}
+		int altDer=altura(it->der);
+		int altIzq=altura(it->izq);
+		int altura=altIzq>altDer? altIzq+1:altDer+1;
+		return altura;
+		/*if(altIzq>altDer){
+			altura=altIzq+1;
+		}else{
+			altura=altDer+1;
+		}*/
+	}
+	
+	
+	int calcularFE(Nodo<T> *it){
+		return altura(it->der)-altura(it->izq);
+	}
+	
+	Nodo<T> *rotacionDerecha(Nodo<T> *X){
+		Nodo<T> *Y=X->izq;
+		X->izq=Y->der;
+		Y->der=X;
+		return Y;
+	}
+	
+	Nodo<T> *rotacionIzquierda(Nodo<T> *X){
+		Nodo<T> *Y=X->der;
+		X->der=Y->izq;
+		Y->izq=X;
+		return Y;
+	}
+	
+	Nodo<T> *balanceo(Nodo<T> *X){
+		int FEX=calcularFE(X);
+		if(FEX>1){//desbalance a la derecha
+			if(calcularFE(X->der)<0){//pivote desbalanceado hacia la izquierda
+				X->der=rotacionDerecha(X->der);
+			}	
+			X=rotacionIzquierda(X);
+		}else if(FEX<-1){//desbalance a la izquierda
+			if(calcularFE(X->izq)>0){//pivote desbalanceado hacia la derecha
+				X->izq=rotacionIzquierda(X->izq);
+			}
+			X=rotacionDerecha(X);
+		}
+		return X;
+	}
 
 	void insert(T value){
 		Nodo<T> *nuevo=new Nodo<T>(value);
 		if(root==NULL){
 			root=nuevo;
 		}else{
-			insertAux(nuevo, root);
+			root=insertAux(nuevo, root);
 		}
 	}
 	
-	void insertAux(Nodo<T> *nuevo, Nodo<T> *it){
-		if(it==NULL||nuevo->value==it->value){
-			return;
+	Nodo<T> * insertAux(Nodo<T> *nuevo, Nodo<T> *it){
+		if(it==NULL){
+			return nuevo;
 		}
-		if(nuevo->value>it->value){
-			if(it->der==NULL){
-				it->der=nuevo;
-				return;
-			}else{
-				insertAux(nuevo, it->der);
-			}
-		}else{
-			if(it->izq==NULL){
-				it->izq=nuevo;
-			}else{
-				insertAux(nuevo, it->izq);
-			}
+		else if(nuevo->value>it->value){
+			it->der=insertAux(nuevo, it->der);
+		}else if(nuevo->value<it->value){
+			it->izq=insertAux(nuevo, it->izq);
 		}
+		it=balanceo(it);
+		return it;
 	}
 	
 	void printInorder(){
@@ -139,7 +181,7 @@ class BST{
 			//aqui borramos
 			if(it->izq==NULL && it->der==NULL){
 				delete it;
-				aRegresar=NULL;
+				return NULL;
 			}else if(it->izq==NULL && it->der!=NULL){
 				aRegresar=it->der;
 				delete it;
@@ -156,7 +198,7 @@ class BST{
 			}
 			
 		}
-		
+		aRegresar=balanceo(aRegresar);
 		return aRegresar;
 	}
 	
@@ -164,22 +206,12 @@ class BST{
 
 
 int main(){
-	BST<int> b;
-	b.insert(710);
-	b.insert(689);
-	b.insert(2000);
-	b.insert(143);
-	b.insert(702);
-	b.insert(1040);
-	b.insert(2099);
-	b.insert(3300);
-	b.insert(70);
+	AVL<int> b;
+	b.insert(7);
+	b.insert(15);
+	b.insert(12);
 	b.printInorder();
 	cout<<endl;
-	//cout<<b.find(1040)<<endl;
-	//cout<<b.find(5)<<endl;
-	b.borrar(710);
-	b.printInorder();
-	cout<<endl;
+	cout<<"El factor de equilibrio de la raiz es: "<<b.calcularFE(b.root)<<endl;
 	return 0;
 }
